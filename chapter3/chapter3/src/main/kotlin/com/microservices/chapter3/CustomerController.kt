217c0db1de8c5dc14f2da9aa28f1f4a9b5c77cc6
ohdowon064 <dhehdnjs03@gmail.com>
2021-07-28 18:16:45 +0900

    customers 맵 생성

--- chapter3/chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ chapter3/chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController {
 
     @GetMapping(path= ["/customer"])
-    fun getCustomer() = "hello from a controller!"
+    fun getCustomer() = Customer(1, "Kotlin")
 }
