a99ba23fca737cb34a951da043104f16f514aac6
ohdowon064 <dhehdnjs03@gmail.com>
2021-07-29 13:29:44 +0900

    RESTful API RequestMapping

--- chapter3/chapter3/src/main/kotlin/com/microservices/chapter3/Chapter3Application.kt
+++ chapter3/chapter3/src/main/kotlin/com/microservices/chapter3/Chapter3Application.kt
@@ -2,9 +2,22 @@ package com.microservices.chapter3
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class Chapter3Application
+class Chapter3Application {
+	companion object {
+		val initialCustomers = arrayOf(Customer(1, "Kotlin"),
+			Customer(2, "Spring"),
+			Customer(3, "Microservices"))
+	}
+	
+	@Bean
+	fun customers() = ConcurrentHashMap<Int, Customer>(
+		initialCustomers.associateBy(Customer::id)
+	)
+}
 
 fun main(args: Array<String>) {
 	runApplication<Chapter3Application>(*args)
--- chapter3/chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ chapter3/chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,12 +1,41 @@
 package com.microservices.chapter3
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class CustomerController {
 
-    @GetMapping(path= ["/customer"])
-    fun getCustomer() = Customer(1, "Kotlin")
+    @Autowired
+    lateinit var customers : ConcurrentHashMap<Int, Customer>
+
+    @GetMapping(path=["/customer"])
+    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
+
+    @GetMapping(path= ["/customer/{id}"])
+    fun getCustomer(
+        @PathVariable id: Int
+    ) = customers[id]
+
+    @PostMapping(path=["/customer"])
+    fun createCustomer(
+        @RequestBody customer: Customer
+    ): String {
+        customers[customer.id] = customer
+        return "${customer.id} customer를 생성했습니다."
+    }
+
+    @DeleteMapping(path=["/customer/{id}"])
+    fun deleteCustomer(@PathVariable id: Int): String {
+        customers.remove(id)
+        return "$id customer를 삭제했습니다."
+    }
+
+    @RequestMapping(path=["/customer/{id}"], method=[RequestMethod.PUT])
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer): String {
+        customers.remove(id)
+        customers[customer.id] = customer
+        return "$id customer를 업데이트 했습니다."
+    }
 }
