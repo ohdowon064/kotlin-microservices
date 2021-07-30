cedafb18a2b4ccc836797d1ab112f7ae110810f3
ohdowon064 <dhehdnjs03@gmail.com>
2021-07-30 13:53:14 +0900

    chapter4 : reactive web

---
+++ chapter4/chapter4/src/test/kotlin/com/microservices/chapter4/Chapter4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
