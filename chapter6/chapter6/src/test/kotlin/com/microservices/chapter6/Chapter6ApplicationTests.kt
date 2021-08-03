478cf80cc72ad3851610d444000895383e2739a3
ohdowon064 <dhehdnjs03@gmail.com>
2021-08-03 16:19:41 +0900

    chapter 6 : 클라우드 네이티브 마이크로서비스

---
+++ chapter6/chapter6/src/test/kotlin/com/microservices/chapter6/Chapter6ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter6
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter6ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
