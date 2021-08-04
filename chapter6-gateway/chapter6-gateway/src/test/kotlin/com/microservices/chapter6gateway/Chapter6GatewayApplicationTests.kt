2b6b1dbdd5a01c42fa1b7e486251bca9b3f560a4
ohdowon064 <dhehdnjs03@gmail.com>
2021-08-04 20:54:59 +0900

    Spring Cloud Gateway, SCG를 이용한 게이트웨이 구현

---
+++ chapter6-gateway/chapter6-gateway/src/test/kotlin/com/microservices/chapter6gateway/Chapter6GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter6gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter6GatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
--- chapter6-microservice/chapter6-microservice/src/main/kotlin/com/microservices/chapter6microservice/controller/GreetingsController.kt
+++ chapter6-microservice/chapter6-microservice/src/main/kotlin/com/microservices/chapter6microservice/controller/GreetingsController.kt
@@ -2,15 +2,22 @@ package com.microservices.chapter6microservice.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/greetings")
 class GreetingsController {
 
     @Value("\${microservice.example.greetings}")
     private lateinit var greetings: String
 
-    @GetMapping(path=["/greetings"])
+    @GetMapping()
     fun greetings() = greetings
+
+    @GetMapping(path=["/info"])
+    fun info(
+        @Value("\${server.port}") port: String
+    ) = "greetings 서비스의 기본 동작 Port: ${port}"
 
 }
