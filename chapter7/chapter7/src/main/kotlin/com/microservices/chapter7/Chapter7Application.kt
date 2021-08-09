1e8052e9a6e8c0d428f54af3753293f4fe0e1961
ohdowon064 <dhehdnjs03@gmail.com>
2021-08-09 17:13:05 +0900

    java -version 명령을 수행하는 간단한 도커파일를 만들고 build한 후 docker run으로 실행해보기

--- chapter7/chapter7/src/main/kotlin/com/microservices/chapter7/Chapter7Application.kt
+++ chapter7/chapter7/src/main/kotlin/com/microservices/chapter7/Chapter7Application.kt
@@ -2,9 +2,17 @@ package com.microservices.chapter7
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Chapter7Application
+
+@RestController
+class GreetingsController {
+	@GetMapping("/greetings")
+	fun greetings() = "hello from a Docker"
+}
 
 fun main(args: Array<String>) {
 	runApplication<Chapter7Application>(*args)
