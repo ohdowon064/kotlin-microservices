eda0d3c0ab81ce9fc18860567a32ef25be48f031
ohdowon064 <dhehdnjs03@gmail.com>
2021-08-12 17:58:30 +0900

    mock 설치 후 모의 값 반환하기

--- chapter9/chapter9/src/test/kotlin/com/microservice/chapter9/controller/CustomerControllerTest.kt
+++ chapter9/chapter9/src/test/kotlin/com/microservice/chapter9/controller/CustomerControllerTest.kt
@@ -1,11 +1,16 @@
 package com.microservice.chapter9.controller
 
+import com.microservice.chapter9.model.Customer
+import com.microservice.chapter9.service.CustomerService
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.reset
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -21,30 +26,41 @@ class CustomerControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @MockBean
+    lateinit var customerService: CustomerService
+
     @Test
     fun `mock mvc should be configured`() {
     }
 
     @Test
     fun `we should GET a customer by id`() {
+        given(customerService.getCustomer(1))
+            .willReturn(Customer(1, "mock customer"))
+
         mockMvc.perform(get("/customer/1"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("\$.id").value(1))
             .andExpect(jsonPath("\$.name").value("Kotlin"))
             .andDo(print())
+
+        reset(customerService)
     }
 
     @Test
     fun `we should GET a list of customers`() {
+        given(customerService.getAllCustomers())
+            .willReturn(listOf(Customer(1, "test"), Customer(2, "mocks")))
+
         mockMvc.perform(get("/customers"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("\$").isArray)
             .andExpect(jsonPath("\$[0].id").value(1))
-            .andExpect(jsonPath("\$[0].name").value("kotlin"))
+            .andExpect(jsonPath("\$[0].name").value("test"))
             .andExpect(jsonPath("\$[1].id").value(2))
-            .andExpect(jsonPath("\$[1].name").value("Spring"))
-            .andExpect(jsonPath("\$[2].id").value(3))
-            .andExpect(jsonPath("\$[2].name").value("Microservice"))
+            .andExpect(jsonPath("\$[1].name").value("mocks"))
             .andDo(print())
+
+        reset(customerService)
     }
 }
